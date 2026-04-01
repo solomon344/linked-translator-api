f397726aca7737d7b9cf0e3b4192458f52391dfd
solomon344 <willamssolomon672@gmail.com>
2026-04-01 18:19:13 +0000

    tries calling config

--- prisma.config.js
+++ prisma.config.js
@@ -1,11 +1,14 @@
-import "dotenv/config";
+import {config} from "dotenv"
 import { defineConfig, env } from "prisma/config";
+
+config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
+  
   datasource: {
     url: env("DATABASE_URL"),
   },
